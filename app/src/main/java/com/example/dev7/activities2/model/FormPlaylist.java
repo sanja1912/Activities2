b7bd2854557a21141284984cfae95f82184a60a3
sanja1912 <sanja.milunovic93@gmail.com>
2017-12-27 10:09:48 +0100

    RxJava2+Room+EventBus

--- app/src/main/java/com/example/dev7/activities2/model/FormPlaylist.java
+++ app/src/main/java/com/example/dev7/activities2/model/FormPlaylist.java
@@ -1,34 +1,29 @@
-package com.example.dev7.activities2;
+package com.example.dev7.activities2.model;
 
 import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
+import android.arch.persistence.room.Room;
 import android.content.DialogInterface;
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.dev7.activities2.R;
+import com.example.dev7.activities2.database.AppDatabase;
+import com.example.dev7.activities2.events.GeneralEvents;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
 import java.text.SimpleDateFormat;
-import java.util.GregorianCalendar;
-import java.net.URI;
-import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.List;
-
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.Fragment;
-
-import org.greenrobot.greendao.query.Query;
 
 public class FormPlaylist extends AppCompatActivity  {
     Button addSong;
@@ -39,8 +34,7 @@ public class FormPlaylist extends AppCompatActivity  {
     int day;
     private EditText songEdit;
     private EditText singerEdit;
-    private SongDao songDao;
-
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +45,21 @@ public class FormPlaylist extends AppCompatActivity  {
         btnDate=findViewById(R.id.btn_datePicker);
         songEdit=findViewById(R.id.edit_text_song);
         singerEdit=findViewById(R.id.edit_text_singer);
+        EventBus.getDefault().register(this);
+        db= Room.databaseBuilder(getApplicationContext(),AppDatabase.class,"movie_and_song")
+                .allowMainThreadQueries()
+                .fallbackToDestructiveMigration()
+                .build();
 
-        Song listOfSong = (Song) getIntent().getSerializableExtra("obj");
-        songEdit.setText(listOfSong.getName());
-        singerEdit.setText(listOfSong.getSinger());
-
-
+        final Song objSong = (Song) getIntent().getSerializableExtra("obj");
+        if(objSong!=null){
+            songEdit.setText(objSong.getSong());
+            singerEdit.setText(objSong.getSinger());
+        }
         long date = System.currentTimeMillis();
         SimpleDateFormat sdf = new SimpleDateFormat("MMM  dd, yyyy h:mm a");
         String dateString = sdf.format(date);
+
         btnDate.setText(dateString);
         btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,8 +71,6 @@ public class FormPlaylist extends AppCompatActivity  {
                 DatePickerDialog pickerDialog=new DatePickerDialog(FormPlaylist.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
-
-
                     }
                 }, year, month, day);
                 pickerDialog.show();
@@ -82,27 +80,30 @@ public class FormPlaylist extends AppCompatActivity  {
         addSong.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DaoSession daoSession=((App)getApplication()).getDaoSession();
-                songDao=daoSession.getSongDao();
-
-                Song song=new Song();
-                song.getId();
-                songDao.getKey(song);
-                song.setName(songEdit.getText().toString());
-                song.setSinger(singerEdit.getText().toString());
-                songDao.insert(song);
-
-
+                if(objSong==null){
+                    Song song=new Song();
+                    song.setSong(songEdit.getText().toString());
+                    song.setSinger(singerEdit.getText().toString());
+                    db.songDao().insertAll(song);
+                } else{
+                    objSong.setSong(songEdit.getText().toString());
+                    objSong.setSinger(singerEdit.getText().toString());
+                    db.songDao().updateAll(objSong);
+                }
                 openDialog();
-
             }
         });
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
-    }
 
+        GeneralEvents.ToolbarEventSong toolbarEvent = new GeneralEvents.ToolbarEventSong();
+        toolbarEvent.name=objSong.getSong();
+        toolbarEvent.singer=objSong.getSinger();
+        EventBus.getDefault().post(toolbarEvent);
+
+    }
 
 
 
@@ -115,29 +116,29 @@ public class FormPlaylist extends AppCompatActivity  {
                 dialogInterface.cancel();
             }
         });
-
         builder.setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-
                 Toast.makeText(getApplicationContext(),"Song successfully saved",Toast.LENGTH_SHORT).show();
                 finish();
             }
         });
-
         AlertDialog alert=builder.create();
         alert.show();
-
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         int id=item.getItemId();
         if(id==android.R.id.home){
             this.finish();
         } return super.onOptionsItemSelected(item);
+    }
+
+   @Subscribe(threadMode = ThreadMode.MAIN)
+      public void setToolbar(GeneralEvents.ToolbarEventSong event){
+        getSupportActionBar().setTitle(event.name+"-"+event.singer);
+
     }
 
 
